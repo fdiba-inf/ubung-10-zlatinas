d3a65a7492e0134011418cf89815071dd1a86bb1
zlatinas <>
2021-12-21 11:49:24 +0000

    z

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,70 +1,55 @@
 package exercise10;
 
-public class Ellipse {
-    private Point startPoint;
-    private double a;
-    private double b;
+public class Ellipse extends Figure {
+    
 
     public Ellipse() {
-        startPoint = new Point(0, 0);
-        a = 1;
-        b = 1;
+      super(new Point(0, 0), 1, 1);
+        
     }
 
     public Ellipse(Point startPoint, double a, double b) {
-        this.startPoint = new Point(startPoint);
-        this.a = a;
-        this.b = b;
+      super(new Point(startPoint), a, b);
     }
 
     public Ellipse(Ellipse otherEllipse) {
-        startPoint = new Point(otherEllipse.startPoint);
-        a = otherEllipse.a;
-        b = otherEllipse.b;
+      super(new Point(otherEllipse.startPoint), otherEllipse.side1,otherEllipse.side2);
+        
     }
 
-    public boolean isValid() {
-        return a > 0 && b > 0;
-    }
-
-    public void initialize() {
-        do {
-            System.out.println("Start point: ");
-            startPoint.initialize();
-            System.out.print("Enter a: ");
-            a = Utils.INPUT.nextDouble();
-            System.out.print("Enter b: ");
-            b = Utils.INPUT.nextDouble();
-        } while (!isValid());
-    }
+   
 
     public double calculatePerimeter() {
-        return Math.PI * (3.0 * (a + b) - Math.sqrt((3.0 * a + b) * (a + 3.0 * b)));
+        return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
 
     public double calculateArea() {
-        return Math.PI * a * b;
+        return Math.PI * side1 * side2;
     }
 
     public String getType() {
-        return (a == b) ? "Circle" : "Ellipse";
+        return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
-    public String toString() {
-        return String.format("%s-[%s, %s], %s, P=%s, A=%s", startPoint, a, b, getType(), calculatePerimeter(), calculateArea());
+    public boolean equal(Figure otherFigure) {
+        if (otherFigure instanceof Ellipse){
+          return super.equal(otherFigure);
+        } else {
+          return false;
+        }
+
+       
     }
-
-    public boolean equal(Ellipse otherEllipse) {
-        boolean sameA = Utils.equals(a, otherEllipse.a);
-        boolean sameB = Utils.equals(b, otherEllipse.b);
-        boolean sameAReversed = Utils.equals(a, otherEllipse.b);
-        boolean sameBReversed = Utils.equals(b, otherEllipse.a);
-
-        return (sameA && sameB) || (sameAReversed && sameBReversed);
-    }
-
+    @Override
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
-        return false;
+        double clickX = click.getX();
+        double clickY = click.getY();
+        double centerX = startPoint.getX() + side1;
+        double centerY = startPoint.getY() + side2;
+        double part1 = Math.pow(clickX - centerX, 2) / Math.pow(side1, 2);
+        double part2 = Math.pow(clickY - centerY, 2) / Math.pow(side2, 2);
+
+
+        return part1 + part2 <= 1;
     }
 }
--- src/exercise10/Rectangle.java
+++ src/exercise10/Rectangle.java
@@ -39,7 +39,12 @@ public class Rectangle extends Figure {
 
     @Override
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
-        return false;
+        double clickX = click.getX();
+        double clickY = click.getY();
+        double startX = startPoint.getX();
+        double startY = startPoint.getY();
+        boolean xInside = startX < clickX && (startX + side1) > clickX;
+        boolean yInside = startY < clickY && (startY + side2) > clickY;
+        return xInside && yInside;
     }
 }
